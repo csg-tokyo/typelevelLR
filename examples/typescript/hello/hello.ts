9810147a8be96219702695679827c06f7fba7561
Matts966 <Matts966@users.noreply.github.com>
2020-01-23 06:02:57 +0900

    Add example & refactor example helloDSL as module

--- examples/typescript/hello/hello.ts
+++ examples/typescript/hello/hello.ts
@@ -1,4 +1,13 @@
 import * as helloDSL from "./helloDSL"
 helloDSL.begin()
     .hello()
-    .
+    .name("OK")
+    .end()
+    .accept(new class visitor extends helloDSL.Visitor {
+        visitHelloWithName(h: helloDSL.HelloWithName) {
+            console.log(h.name)
+        }
+        visitSimpleHello() {
+            console.log("Simple")
+        }
+    }())
--- examples/typescript/hello/helloDSL.ts
+++ examples/typescript/hello/helloDSL.ts
@@ -1,9 +1,80 @@
-export class Visitor {
-    visitSimpleHello(host : SimpleHello) {}
-    visitHelloWithName(host : HelloWithName) {}
+type Length<T extends unknown[]> = T['length']
+type Prepend<Elm, T extends unknown[]> = ((
+    arg: Elm,
+    ...rest: T
+) => void) extends ((...args: infer T2) => void)
+    ? T2
+    : never
+
+type Rest<T extends unknown[]> = ((
+    ...rest: T
+) => void) extends ((head: unknown, ...args: infer T2) => void)
+    ? T2
+    : never
+declare const None: unique symbol
+type None = typeof None
+type Head<T extends unknown[]> = Length<T> extends 0 ? None : T[0]
+type AddUnknownRest<Tuple extends unknown[], Result extends unknown[] = [...unknown[]]> = {
+    empty: Result,
+    nonEmpty: ((..._: Tuple) => unknown) extends ((_: infer First, ..._1: infer Next) => unknown)
+      ? Prepend<First, AddUnknownRest<Rest<Tuple>, Result>>
+      : never
+}[
+    Tuple extends [unknown, ...unknown[]]
+      ? 'nonEmpty'
+      : 'empty'
+]
+type CompareLength<Left extends any[], Right extends any[]> = {
+    fitBoth: 'equal'
+    fitLeft: 'shorterLeft'
+    fitRight: 'shorterRight'
+    unfit: ((..._: Left) => any) extends ((_: any, ..._1: infer LeftRest) => any) ?
+         ((..._: Right) => any) extends ((_: any, ..._1: infer RightRest) => any) ?
+            CompareLength<LeftRest, RightRest>
+        : never
+        : never
+}[
+    Left['length'] extends Right['length'] ? 'fitBoth' :
+    Left extends [] ? 'fitLeft' :
+    Right extends [] ? 'fitRight' :
+    'unfit'
+]
+type StartsWith<Tuple extends unknown[], Tuple2 extends unknown[]> = {
+    false: 0
+    empty: 1,
+    nonEmpty: Head<Tuple> extends Head<Tuple2>
+        ? StartsWith<Rest<Tuple>, Rest<Tuple2>>
+        : 0
+}[
+    CompareLength<Tuple, Tuple2> extends 'shorterLeft'
+        ? 'false'
+        : IsFinite<Tuple2, 'finite', 'infinite'> extends 'infinite'
+            ? 'false'
+            : Tuple2 extends [unknown, ...unknown[]]
+                ? 'nonEmpty'
+                : 'empty'
+]
+type IsFinite<Tuple extends unknown[], Finite, Infinite> = {
+    empty: Finite
+    nonEmpty: ((..._: Tuple) => unknown) extends ((_: infer First, ..._1: infer Rest) => unknown)
+      ? IsFinite<Rest, Finite, Infinite>
+      : never
+    infinite: Infinite
+}[
+    Tuple extends [] ? 'empty' :
+    Tuple extends (infer Element)[] ?
+    Element[] extends Tuple ?
+      'infinite'
+    : 'nonEmpty'
+    : never
+]
+
+export abstract class Visitor {
+    abstract visitSimpleHello(host : SimpleHello)
+    abstract visitHelloWithName(host : HelloWithName)
 }
 
-export class ConstVisitor {
+class DefaultVisitor extends Visitor {
     visitSimpleHello(host : SimpleHello) {
         console.log("hello")
     }
@@ -13,13 +84,17 @@ export class ConstVisitor {
 }
 
 abstract class Start {
-    abstract accept(v : Visitor): void
-    abstract accept(v : ConstVisitor): void
+    abstract accept(v? : Visitor): void
 }
+
 export class SimpleHello extends Start {
-    accept(v : Visitor): void
-    accept(v : ConstVisitor): void
-    accept(v : Visitor | ConstVisitor) { v.visitSimpleHello(this) }
+    accept(v? : Visitor) {
+        if (v) {
+            v.visitSimpleHello(this)
+        } else {
+            new DefaultVisitor().visitSimpleHello(this)
+        }
+    }
 }
 
 export class HelloWithName {
@@ -27,9 +102,13 @@ export class HelloWithName {
     constructor(name : string) {
         this.name = name
     }
-    accept(v : Visitor): void
-    accept(v : ConstVisitor): void
-    accept(v : Visitor | ConstVisitor) { v.visitHelloWithName(this) }
+    accept(v? : Visitor) {
+        if (v) {
+            v.visitHelloWithName(this)
+        } else {
+            new DefaultVisitor().visitHelloWithName(this)
+        }
+    }
 }
 
 type Node = Node1 | Node2 | Node3 | Node4
@@ -54,94 +133,7 @@ class Node4 {
     private _node4Brand: boolean = true
 }
 
-type Length<T extends unknown[]> = T['length']
-
-type Prepend<Elm, T extends unknown[]> = ((
-    arg: Elm,
-    ...rest: T
-) => void) extends ((...args: infer T2) => void)
-    ? T2
-    : never
-
-type Rest<T extends unknown[]> = ((
-    ...rest: T
-) => void) extends ((head: unknown, ...args: infer T2) => void)
-    ? T2
-    : never
-
-declare const None: unique symbol
-type None = typeof None
-type Head<T extends unknown[]> = Length<T> extends 0 ? None : T[0]
-
-type AddUnknownRest<Tuple extends unknown[], Result extends unknown[] = [...unknown[]]> = {
-    empty: Result,
-    nonEmpty: ((..._: Tuple) => unknown) extends ((_: infer First, ..._1: infer Next) => unknown)
-      ? Prepend<First, AddUnknownRest<Rest<Tuple>, Result>>
-      : never
-}[
-    Tuple extends [unknown, ...unknown[]]
-      ? 'nonEmpty'
-      : 'empty'
-]
-
-type CompareLength<Left extends any[], Right extends any[]> = {
-    fitBoth: 'equal'
-    fitLeft: 'shorterLeft'
-    fitRight: 'shorterRight'
-    unfit: ((..._: Left) => any) extends ((_: any, ..._1: infer LeftRest) => any) ?
-         ((..._: Right) => any) extends ((_: any, ..._1: infer RightRest) => any) ?
-            CompareLength<LeftRest, RightRest>
-        : never
-        : never
-}[
-    Left['length'] extends Right['length'] ? 'fitBoth' :
-    Left extends [] ? 'fitLeft' :
-    Right extends [] ? 'fitRight' :
-    'unfit'
-]
-
-type StartsWith<Tuple extends unknown[], Tuple2 extends unknown[]> = {
-    false: 0
-    empty: 1,
-    nonEmpty: Head<Tuple> extends Head<Tuple2>
-        ? StartsWith<Rest<Tuple>, Rest<Tuple2>>
-        : 0
-}[
-    CompareLength<Tuple, Tuple2> extends 'shorterLeft'
-        ? 'false'
-        : IsFinite<Tuple2, 'finite', 'infinite'> extends 'infinite'
-            ? 'false'
-            : Tuple2 extends [unknown, ...unknown[]]
-                ? 'nonEmpty'
-                : 'empty'
-]
-
-type IsFinite<Tuple extends unknown[], Finite, Infinite> = {
-    empty: Finite
-    nonEmpty: ((..._: Tuple) => unknown) extends ((_: infer First, ..._1: infer Rest) => unknown)
-      ? IsFinite<Rest, Finite, Infinite>
-      : never
-    infinite: Infinite
-}[
-    Tuple extends [] ? 'empty' :
-    Tuple extends (infer Element)[] ?
-    Element[] extends Tuple ?
-      'infinite'
-    : 'nonEmpty'
-    : never
-]
-
-interface ProxyConstructor {
-    revocable<T extends object>(target: T, handler: ProxyHandler<T>): {
-        proxy: T
-        revoke: () => void
-    }
-    new <T extends object>(target: T, handler: ProxyHandler<T>): T
-}
-declare var Proxy: ProxyConstructor
-declare interface ProxyHandler<T> {}
-
-export type Fluent<Stack extends unknown[]> = (
+type Fluent<Stack extends unknown[]> = (
     StartsWith<Stack, [Node2]> extends 1 ?
     { end: () => Start } :
     {}
@@ -208,8 +200,20 @@ function isNode41(arg: any): arg is AddUnknownRest<[Node4, Node1]> {
         && arg[1] && arg[1]._node1Brand
 }
 
-function begin(): Fluent<[Node1]> {
+export function begin(): Fluent<[Node1]> {
     return new FluentImpl() as any
 }
 
-begin().hello().name("ok").end().accept(new ConstVisitor)
+begin().hello().name("ok").end().accept(new DefaultVisitor)
+
+begin()
+    .hello()
+    .end()
+    .accept(new class visitor extends Visitor {
+        visitHelloWithName(h: HelloWithName) {
+            console.log(h.name)
+        }
+        visitSimpleHello() {
+            console.log("Simple")
+        }
+    }())
