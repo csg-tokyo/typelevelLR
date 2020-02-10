cd82330e9a927978fbe41fee83426f5777315d3e
Matts966 <Matts966@users.noreply.github.com>
2020-02-10 15:23:29 +0900

    Use parens instead of $ to fix a but that ignore terminals which start with 'e'
    (cherry picked from commit aa80c2ac53884ef1d35b9b02e27e558b7311398b)

--- src/SyntaxParser.hs
+++ src/SyntaxParser.hs
@@ -56,7 +56,7 @@ parseRule = do
   return (Rule name lhs rhs)
 
 parseDerivation :: (Stream s m Char) => ParsecT s u m [Symbol]
-parseDerivation  = try $ [] <$ string "eps"
+parseDerivation  = try ([] <$ string "eps")
                 <|> trySepBy (ws' *> parseSymbol) (notFollowedBy (ws' *> parseIdentifier *> ws' *> (string ":")))
 
 parseSymbol :: (Stream s m Char) => ParsecT s u m Symbol
